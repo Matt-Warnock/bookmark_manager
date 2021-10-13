f48c85d3edcfc4df1177857c80c2c4305685505d
MattW <mattnwarnock@yahoo.co.uk>
2021-10-13 18:10:19 +0100

    create link with title

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,27 +1,41 @@
 require 'bookmark'
 
 describe Bookmark do
+
   describe '.all' do
     it 'returns a list of bookmarks' do
       connection = PG.connect(dbname: 'bookmark_manager_test')
-  
+
       # Add the test data
-      Bookmark.create(url: "http://www.makersacademy.com")
-      Bookmark.create(url: "http://www.destroyallsoftware.com")
-      Bookmark.create(url: "http://www.google.com")
-  
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
+
       bookmarks = Bookmark.all
-  
-      expect(bookmarks).to include('http://www.makersacademy.com')
-      expect(bookmarks).to include('http://www.destroyallsoftware.com')
-      expect(bookmarks).to include('http://www.google.com')
+
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
   describe '.create' do
-    it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://www.example.org')
-      expect(Bookmark.all).to include('http://www.example.org')
+    it 'creates a new bookmark title' do
+      bookmark = Bookmark.create(url: 'http://www.example.org', title: 'Test Bookmark')
+
+      title = bookmark.title
+
+      expect(title).to eq('Test Bookmark')
+    end
+
+    it 'creates a new bookmark url' do
+      bookmark = Bookmark.create(url: 'http://www.example.org', title: 'Test Bookmark')
+
+      url = bookmark.url
+
+      expect(url).to eq('http://www.example.org')
     end
   end
 end
