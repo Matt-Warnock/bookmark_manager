1d03a163e43ac8e3e10e658fd1d66ad17f79df6f
Douglas <douglaskerins@gmail.com>
2021-10-13 15:44:41 +0100

    refactoring adding new bookmark

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -6,15 +6,22 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
   
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      Bookmark.create(url: "http://www.makersacademy.com")
+      Bookmark.create(url: "http://www.destroyallsoftware.com")
+      Bookmark.create(url: "http://www.google.com")
   
       bookmarks = Bookmark.all
   
       expect(bookmarks).to include('http://www.makersacademy.com')
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new bookmark' do
+      Bookmark.create(url: 'http://www.example.org')
+      expect(Bookmark.all).to include('http://www.example.org')
     end
   end
 end
